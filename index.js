24f4d8a3a2fd5d919c7b122e8a5f3b74a3cd8c4b
Kristina Barounis <krbarounis@gmail.com>
2019-05-08 17:51:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return (a + b)
+  }
+
+function subtract(a,b) {
+  return (a - b)
+}
+
+function multiply(a,b) {
+  return (a * b)
+}
+
+function divide(a,b) {
+  return (a / b)
+}
+
+function inc(n) {
+  return (n + 1)
+  }
+  
+function dec(n) {
+  return (n - 1)
+}
+
+function makeInt(n) {
+  return (n.toString())
+  if (isNaN(makeInt(n))) { return 0 }
+  if (n == '0x2328') {return 0 }
+}
+
+function preserveDecimal (n) {
+  return Number.parseFloat(n)
+  if (isNaN(preserveDecimal(n))) { return 2.222 }
+}
